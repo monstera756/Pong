8f76d0531b5c511c3c525e9d9d0abc9ed5ede2db
monstera756 <aleksandra.kozlowska756@gmail.com>
2024-02-18 22:40:12 +0100

    initialization window added (sdl)

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -1,20 +1,31 @@
-// Pong.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <SDL.h>
+#include <stdio.h>
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	SDL_Window* window = NULL; //window
+
+	SDL_Surface* screenSurface = NULL; //surface contained by window
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "0";
+	}
+	else //create window
+	{
+		window = SDL_CreateWindow("InitWindow", 100, 100, 100, 100, SDL_WINDOW_FULLSCREEN);
+
+		if (window == NULL)
+		{
+			std::cout << "0";
+		}
+		SDL_Delay(3000);
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
